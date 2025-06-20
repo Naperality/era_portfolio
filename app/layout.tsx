c410c413dd9b747ae00c8b69ac68f6791214e2e8
Naperality <napolytan@gmail.com>
2025-06-21 00:34:35 +0800

    Correct URL for OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata = {
     siteName: "Era Portfolio",
     images: [
       {
-        url: "/og-image.png",
+        url: "https://era-portfolio-sigma.vercel.app/og-image.png",
         width: 1200,
         height: 630,
         alt: "Era's Artwork",
