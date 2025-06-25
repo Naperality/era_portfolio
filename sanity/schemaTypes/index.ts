0cd8dd79428de7ddae83368a217b6487eedf3075
Naperality <napolytan@gmail.com>
2025-06-26 02:34:27 +0800

    Revert

--- sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-// /sanity/schema.ts
-import { galleryItem } from './galleryItem'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [galleryItem],
-}
